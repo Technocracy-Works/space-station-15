8e0471c20d1ced99e9e5b9ca37e178cc10286237
Swino4ka <83330628+Swino4ka@users.noreply.github.com>
2026-06-28 19:17:57 +0200

    Крутые плюшки

--- src/main/java/org/technocracy/spacestation/item/SqueakyPlushieItem.java
+++ src/main/java/org/technocracy/spacestation/item/SqueakyPlushieItem.java
@@ -1,5 +1,8 @@
 package org.technocracy.spacestation.item;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,15 +12,10 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 public class SqueakyPlushieItem extends Item {
 
     private static final int COOLDOWN_TICKS = 15;
     private static final float VOLUME = 1.0f;
-    private static final Map<UUID, Long> LAST_SQUEAK_TICK = new HashMap<>();
 
     private final SoundEvent[] squeakSounds;
 
@@ -33,33 +31,48 @@ public class SqueakyPlushieItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getStackInHand(hand);
 
-        if (world.isClient()) {
-            return TypedActionResult.success(stack);
+        player.getItemCooldownManager().set(this, COOLDOWN_TICKS);
+
+        if (!world.isClient()) {
+            SoundEvent sound = squeakSounds[world.getRandom().nextInt(squeakSounds.length)];
+            float pitch = 0.85f + world.getRandom().nextFloat() * 0.3f;
+
+            world.playSound(
+                    null,
+                    player.getX(), player.getY(), player.getZ(),
+                    sound,
+                    SoundCategory.PLAYERS,
+                    VOLUME, pitch
+            );
+
+            player.swingHand(hand, true);
         }
-
-        long now = world.getTime();
-        Long lastTick = LAST_SQUEAK_TICK.get(player.getUuid());
-        if (lastTick != null && now - lastTick < COOLDOWN_TICKS) {
-            return TypedActionResult.fail(stack);
-        }
-
-        SoundEvent sound = squeakSounds[world.getRandom().nextInt(squeakSounds.length)];
-        float pitch = 0.85f + world.getRandom().nextFloat() * 0.3f;
-
-        world.playSound(
-                null,
-                player.getX(),
-                player.getY(),
-                player.getZ(),
-                sound,
-                SoundCategory.PLAYERS,
-                VOLUME,
-                pitch
-        );
-
-        LAST_SQUEAK_TICK.put(player.getUuid(), now);
-        player.swingHand(hand, true);
 
         return TypedActionResult.success(stack);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if (!attacker.getWorld().isClient() && attacker instanceof PlayerEntity player) {
+            float damage = (float) player.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+            target.heal(damage);
+
+            SoundEvent sound = squeakSounds[attacker.getRandom().nextInt(squeakSounds.length)];
+            attacker.getWorld().playSound(
+                    null,
+                    attacker.getX(), attacker.getY(), attacker.getZ(),
+                    sound,
+                    SoundCategory.PLAYERS,
+                    VOLUME, 1.0f
+            );
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean canMine(net.minecraft.block.BlockState state, World world,
+                           net.minecraft.util.math.BlockPos pos, PlayerEntity miner) {
+        return false;
     }
 }
